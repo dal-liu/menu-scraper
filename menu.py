3022834da04225cff2934b8fa46a95f4fd657112
dal-liu <daniel.albert.liu@gmail.com>
2023-08-07 23:08:15 +0800

    Improved finding the timeout text

--- menu.py
+++ menu.py
@@ -6,7 +6,35 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def create_menu():
-    pass
+    menu = {}
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--blink-settings=imagesEnabled=false")
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    driver.get("https://dineoncampus.com/northwestern/whats-on-the-menu")
+    # Wait for dropdown to load and extract it
+    dropdown_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "menu-location-selector__BV_toggle_")))
+    # Extract list of all dining halls from the dropdown
+    dropdown_options = driver.find_elements(By.XPATH, "//ul[@id='building_6113ef5ae82971150a5bf8ba']//li/button")
+    # Iterate through dining halls
+    for option in dropdown_options:
+        dropdown_menu.click()
+        try:
+            # Wait for dropdown menu to load and then click on an option
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(option)).click()
+            # Wait until period tabs have loaded and then extract
+            WebDriverWait(driver, 30).until(EC.any_of(EC.presence_of_all_elements_located((By.XPATH, "//ul[@class='nav nav-tabs']//li/a")),
+                                                      EC.text_to_be_present_in_element((By.XPATH, "//div[@class='loading-content_loadingText_22OQi']"),
+                                                                                       "Sorry, we weren't able to find menus for this location for the day you selected.")))
+        except:
+            continue
+        # Extract the tabs
+        menu[option.get_attribute("textContent").strip()] = _create_periods_list(driver)
+
+    driver.quit()
+    return menu
 
 
 def _create_periods_list(driver):
@@ -18,8 +46,8 @@ def _create_periods_list(driver):
                 WebDriverWait(driver, 3).until(EC.element_to_be_clickable(tab)).click()
                 table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@role='tabpanel' and @aria-hidden='false']")))
                 WebDriverWait(table, 30).until(EC.any_of(EC.presence_of_element_located((By.XPATH, "//div[@class='row menu-period-dropdowns']")),
-                                                         EC.text_to_be_present_in_element((By.XPATH, "//div[@class='loading-content_loadingText_22OQi']"),
-                                                                                          "Sorry, we weren't able to find menus for this location for the day you selected.")))
+                                                         EC.text_to_be_present_in_element((By.XPATH, "div/div[@class='loading-content_loadingText_22OQi']"),
+                                                                                          "Sorry, we weren't able to find menus for this location for the day you selected."))) # TODO fix this and add retry
             except:
                 continue
             stations = table.find_elements(By.TAG_NAME, "table")
@@ -64,37 +92,6 @@ def _create_item(item):
         "portion": properties[1].text,
         "calories": int(properties[2].text)
     }
-
-
-menu = {}
-
-driver = webdriver.Chrome()
-
-driver.get("https://dineoncampus.com/northwestern/whats-on-the-menu")
-
-# Wait for dropdown to load and extract it
-dropdown_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "menu-location-selector__BV_toggle_")))
-# Extract list of all dining halls from the dropdown
-dropdown_options = driver.find_elements(By.XPATH, "//ul[@id='building_6113ef5ae82971150a5bf8ba']//li/button")
-# Iterate through dining halls
-for option in dropdown_options:
-    dropdown_menu.click()
-    # Dict with keys of time periods and values of the stations and food
-    dining_hall = []
-
-    try:
-        # Wait for dropdown menu to load and then click on an option
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(option)).click()
-        # Wait until period tabs have loaded and then extract
-        WebDriverWait(driver, 30).until(EC.any_of(EC.presence_of_all_elements_located((By.XPATH, "//ul[@class='nav nav-tabs']//li/a")),
-                                                  EC.text_to_be_present_in_element((By.XPATH, "//div[@class='loading-content_loadingText_22OQi']"),
-                                                                                   "Sorry, we weren't able to find menus for this location for the day you selected.")))
-    except:
-        continue
-    # Extract the tabs
-    menu[option.get_attribute("textContent").strip()] = _create_periods_list(driver)
     
 with open("menu.json", "w") as json_file:
-    json.dump(menu, json_file, indent=4)
-
-driver.quit()
+    json.dump(create_menu(), json_file, indent=4)
